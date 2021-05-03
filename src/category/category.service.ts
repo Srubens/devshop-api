96f44d8c66a38bc1b2c4ed6e8726045dac8971d4
Rubens Filipe <rubensfilipe6@gmail.com>
2021-05-03 01:07:32 -0300

    add o graphql no sistema

---
+++ src/category/category.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { Category } from "./dto/category";
+
+@Injectable()
+export class CategoryService{
+    async findAll():Promise<Category[]>{
+        return [
+            {id:'a', name:'teste'}
+        ]
+    }
+}
